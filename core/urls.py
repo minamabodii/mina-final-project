d46925cc01c45e2b09b7f9acf08b8fd6207f9620
mina <minamabodi1313@gmail.com>
2025-08-01 23:32:27 +0330

    uninstall drf yasg and djangaorestframework

--- core/urls.py
+++ core/urls.py
@@ -16,20 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
 
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title='API Docs',
-        default_version='v1',
-    )
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogs.urls')),
-    path('swagger/', schema_view.with_ui('swagger',
-         cache_timeout=0), name='schema-swagger-ui'),
 ]
